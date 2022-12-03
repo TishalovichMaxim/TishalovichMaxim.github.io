3ff4ed458f0a178a6e71d9de26a9fe9463fa842c
Maxim Tishalovich <tishalovichm@gmail.com>
2022-12-03 23:11:11 +0300

    adding our team section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Col, Container, Row, Breadcrumb, Image, Carousel, Stack} from 'react-bootstrap';
 import Navibar from './Components/Navibar.js'
 import PortalDescription from './Components/PortalDescription';
+import KitchenSinkExample from './Components/OurTeam';
 
 function App() {
   return (
     <>
       <Navibar/>    
       <PortalDescription/>
+      <KitchenSinkExample/>
     </>
   );
 }
---
+++ src/Components/OurTeam.js
@@ -0,0 +1,73 @@
+import {Card, ListGroup, Container, Row, Col} from 'react-bootstrap';
+function KitchenSinkExample() {
+  return (
+
+    <Container>
+        <Row>
+            <h1 className='text-center'>Our team</h1>
+        </Row>
+        <Row className='justify-content-center align-items-center'>
+            <Col className='d-flex justify-content-center align-items-center'>
+                <Card style={{ width: '18rem' }} >
+                <Card.Img variant="top" src="https://www.hdwallpapers.in/download/demon_slayer_black_and_white_kyojuro_rengoku_with_sword_hd_anime-1920x1080.jpg" />
+                <Card.Body>
+                    <Card.Title>Tishalovich Maxim</Card.Title>
+                    <Card.Text>
+                    The most important developer of our team. Best chess player after cmak33. 
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>
+                        <Card.Link href="#">GitHub</Card.Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Card.Link href="#">VK</Card.Link>
+                    </ListGroup.Item>
+                </ListGroup>
+                </Card>
+            </Col>
+            <Col className='d-flex justify-content-center align-items-center'>
+                <Card style={{ width: '18rem' }}>
+                <Card.Img width="" variant="top" src="https://wallpaperboat.com/wp-content/uploads/2020/10/10/56411/anime-black-and-white-02.jpg" />
+                <Card.Body>
+                    <Card.Title>Eremeychik Kirill</Card.Title>
+                    <Card.Text>
+                    Java cenior developer. Master of Sports in swimming.  
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>
+                        <Card.Link href="#">GitHub</Card.Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Card.Link href="#">VK</Card.Link>
+                    </ListGroup.Item>
+                </ListGroup>
+                </Card>
+            </Col>
+
+            <Col className='d-flex justify-content-center align-items-center'>
+                <Card style={{ width: '18rem' }}>
+                <Card.Img width="" variant="top" src="https://wallpapercave.com/wp/wp3110694.jpg" />
+                <Card.Body>
+                    <Card.Title>Gonzarevich Ilya</Card.Title>
+                    <Card.Text>
+                    Assembler developer. Basketball MVP.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>
+                        <Card.Link href="#">GitHub</Card.Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Card.Link href="#">VK</Card.Link>
+                    </ListGroup.Item>                    
+                </ListGroup>
+                </Card>
+            </Col>            
+        </Row>
+    </Container>
+  );
+}
+
+export default KitchenSinkExample;
