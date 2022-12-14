1922719632c6a81c8cfb87856ee6fa25e0353788
hahahalol9 <cmak7543@mail.ru>
2022-12-14 23:34:40 +0300

    fix: map alignment

--- src/Components/WriterComponents/WriterMap.jsx
+++ src/Components/WriterComponents/WriterMap.jsx
@@ -4,27 +4,32 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import markerImg from '../../Images/marker.png';
 import "./writerMap.css";
+import { Card, ListGroup, Container, Row, Col } from 'react-bootstrap';
 
 function WriterMap(props){
     return (
-        <div >
-            <div className="title">{props.place}</div>
-        <Map  mapLib={maplibregl}
-            initialViewState={{
-                longitude: props.coords.longitude,
-                latitude: props.coords.latitude,
-                zoom: 14
-            }}
-            style={{ width: "80%", height: "90vh" }}
-            mapStyle={process.env.REACT_APP_MAPS_REF}
-        >
-                <NavigationControl position="top-left" />
-                <Marker latitude={props.coords.latitude} longitude={props.coords.longitude} color='#61dbfb'>
-                    <img src={markerImg} widht="50"
-                        height="50"/>
-                </Marker>
-            </Map>
-            </div>
+        <Container>
+            <Row>
+                <div className="title">{props.place}</div>
+            </Row>
+            <Row className='justify-content-center align-items-center'>
+            <Map  mapLib={maplibregl}
+                initialViewState={{
+                    longitude: props.coords.longitude,
+                    latitude: props.coords.latitude,
+                    zoom: 14
+                }}
+                style={{ width: "80%", height: "90vh" }}
+                mapStyle={process.env.REACT_APP_MAPS_REF}
+            >
+                    <NavigationControl position="top-left" />
+                    <Marker latitude={props.coords.latitude} longitude={props.coords.longitude} color='#61dbfb'>
+                        <img src={markerImg} widht="50"
+                            height="50"/>
+                    </Marker>
+                    </Map>
+                </Row>
+            </Container>
     );
 }
 
