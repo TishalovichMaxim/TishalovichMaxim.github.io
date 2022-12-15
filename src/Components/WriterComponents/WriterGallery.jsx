04fabb572e2515752bf99a85dd7218ead6ddf8ef
Maxim Tishalovich <tishalovichm@gmail.com>
2022-12-15 21:07:47 +0300

    galery changing

--- src/Components/WriterComponents/WriterGallery.jsx
+++ src/Components/WriterComponents/WriterGallery.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+import '../../App.css';
 
 function Gallery(props) {
     return (
-        <Container>
-						<div class="row">
-							<div class="col-md-1 mx-auto mt-5 mb-5">
-							<label for="carousel" class="form-label display-5 text-danger">Галерея</label>
-							</div>
-						</div>
-					<Row>
-						<Col>							
-							<Carousel id="carousel">
-							{props.images.map((img,i)=>
+		<>
+			<Container className='py-5 my-3 rounded'>
+				<Row className="align-items-center justify-content-center">
+					<Card style={{ width: '80rem' }} className="pb-5">
+						<Container className>
+							<Row className="text-center align-items-center justify-content-center my-4">
+								<Col className='bg-dark rounded-pill mx-2 py-3 text-light' sm={10}><h2>Галерея</h2></Col>
+							</Row>
+							<Row className="align-items-center justify-content-center">
+								<Col xs lg="10">
+									<Carousel id="carousel">
+										{props.images.map((img,i)=>
 											<Carousel.Item>
-													<img
-															className="d-block w-100"
-															src={img}
-															alt={i}
-													/>
-											</Carousel.Item>
-									)}
-							</Carousel>
-							<br></br>
-						</Col>
-					</Row>
-				</Container>
+												<img
+													rounded
+													className="d-block w-100"
+													src={img}
+													alt={i}
+												/>
+											</Carousel.Item>)}
+									</Carousel>
+								</Col>
+							</Row>
+						</Container>
+					</Card>
+				</Row>
+			</Container>
+		</>
     );
 }
 
