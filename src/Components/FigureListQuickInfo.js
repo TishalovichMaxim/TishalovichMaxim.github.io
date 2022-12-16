bd7c923fd63c4875cd638fd8948e6c3aa7a80664
hahahalol9 <cmak7543@mail.ru>
2022-12-16 14:30:18 +0300

    feat: writer name translation

--- src/Components/FigureListQuickInfo.js
+++ src/Components/FigureListQuickInfo.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 
 function FigureListQuickInfo(props) {
+	const { t, i18n } = useTranslation();
     return (
         <>
 			<div class="card bg-light mt-4 mb-3">
 						<img class="card-img-top" src={props.writer.photo[0]}/>
-						<div class="card-body">
-						<h5 class="card-title">{props.writer.name}</h5>
+				<div class="card-body">
+					<h5 class="card-title">{t(props.writer.name)}</h5>
 					<p class="card-text">{props.writer.smallDescription}</p>
 					<Link to={"/writerPage/" + props.index}>полная информация</Link>
 						</div>
